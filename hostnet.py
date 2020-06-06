25cc36f827dd9de95f70d1933c469935d2a1f428
nickolanick <bilayev@ucu.edu.ua>
2020-06-06 05:11:09 -0700

    Basic methods structure

--- hostnet.py
+++ hostnet.py
@@ -8,24 +8,37 @@ import os
 
 
 class RemoteNode(Node):
-	pass
+	ssh_command = ['ssh','-o', 'ForwardAgent=yes', '-tt']
+        def __init__(self,name,serverIp="localhost",user=None,**kwargs):
+                Node.__init__(self,name, **kwargs )
+	def startShell( self, *args, **kwargs ):		
+		pass	
 
 class RemoteHost(RemoteNode):
-	pass
+        pass
 
 class RemoteLink( Link ):
-	pass
-
+        def __init__(self,node1, node2,**kwargs):
+                Link.__init__(self,node1,node2)
+	def makeIntfPair( self, intfname1, intfname2, addr1=None, addr2=None,node1=None, node2=None, deleteIntfs=True   ):
+		pass
+	def makeTunnel( self, node1, node2, intfname1, intfname2, addr1=None, addr2=None ):
+		pass
+	def moveIntf( intf, node ):
+		pass
 
 def basicTest():
-	net = Mininet( host=RemoteHost, link=link )
-	h1 = net.addHost( 'h1')
-	h2 = net.addHost( 'h2', server=remote )
-	net.addLink( h1, h2 )
-	net.start()
-	net.pingAll()
-	net.stop()
+        remote   = "localhost"
+        username = "mininet"
+        link     = RemoteLink
+        net = Mininet( host=RemoteHost, link=link )
+        h1 = net.addHost( 'h1')
+        h2 = net.addHost( 'h2', serverIp=remote,user=username)
+        net.addLink( h1, h2 )
+        net.start()
+        net.pingAll()
+        net.stop()
 
 
 if __name__=="__main__":
-	basicTest()
+        basicTest()
